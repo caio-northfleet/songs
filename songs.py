1f769c9ad014f1a6b690616ea0b7b9ff2d07c2c7
Caio Northfleet <caio.northfleet@gmail.com>
2018-02-12 17:39:07 +0000

    small refactor for the search songs call

--- songs.py
+++ songs.py
@@ -16,41 +16,48 @@ def get_songs_collection():
     return mongo.db[Const.SONGS_COLLECTION]
 
 
-@app.route('/songs', methods=['GET'])
-def get_all_songs():
+def perform_search_songs(filter):
     output = []
-    for song in get_songs_collection().find():
+    for song in get_songs_collection().find(filter):
       song_id = str(song.pop('_id'))
       song['song_id'] = song_id
       output.append(song)
     return jsonify({'result': output})
 
 
-@app.route('/songs/avg/difficulty', methods=['GET'])
-def get_avg_difficulty():
-
-    aggregate_expression = []
-
-    level = request.args.get('level')
-    if level is not None:
-        aggregate_expression.append({'$match': {'level': int(level)}})
-    aggregate_expression.append({'$group': {'_id': 'null', 'average': {'$avg': "$difficulty"}}})
-
-    result = list(get_songs_collection().aggregate(aggregate_expression))
-
-    output = {'average_difficulty': result[0]['average'] if len(result) > 0 else -1}
-    return jsonify({'result': output})
+@app.route('/songs', methods=['GET'])
+def get_all_songs():
+    filter = {}
+    return perform_search_songs(filter)
 
 
 @app.route('/songs/search', methods=['GET'])
 def search_songs():
+    message = request.args.get('message')
+    if message is not None:
+        filter = {'$or': [
+            {'artist': {'$regex': message , '$options': 'i'}},
+            {'title': {'$regex': message , '$options': 'i'}}
+        ]}
+        return perform_search_songs(filter)
+    return jsonify({'result': []})
 
-    return jsonify({'result': 'wip'})
+
+@app.route('/songs/avg/difficulty', methods=['GET'])
+def get_avg_difficulty():
+    aggregate_expression = []
+    level = request.args.get('level')
+    if level is not None:
+        aggregate_expression.append({'$match': {'level': int(level)}})
+    aggregate_expression.append({'$group': {'_id': 'null', 'average': {'$avg': '$difficulty'}}})
+
+    result = list(get_songs_collection().aggregate(aggregate_expression))
+    output = {'average_difficulty': result[0]['average'] if len(result) > 0 else -1}
+    return jsonify({'result': output})
 
 
 @app.route('/songs/rating', methods=['POST'])
 def add_song_rating():
-
     return jsonify({'result': 'wip'})
 
 
